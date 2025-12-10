ce7c78f26d7d3a56354612396ef85793aef00eb5
lukasjarutis <lukas.jarutis@stud.vilniustech.lt>
2025-12-10 05:42:28 +0200

    вуд

--- KursinisApp/app/src/main/java/com/example/kursinisapp/Utils/Constants.java
+++ KursinisApp/app/src/main/java/com/example/kursinisapp/Utils/Constants.java
@@ -1,7 +1,7 @@
 package com.example.kursinisapp.Utils;
 
 public class Constants {
-    public static final String HOME_URL = "http://10.172.247.204:8888/";
+    public static final String HOME_URL = "http://192.168.1.162:8080/";
     public static final String VALIDATE_USER_URL = HOME_URL + "validateUser";
     public static final String GET_ALL_RESTAURANTS_URL = HOME_URL + "allRestaurants";
     public static final String CREATE_BASIC_USER_URL = HOME_URL + "insertBasic";
--- Prif233/app/src/main/java/com/example/prif233/model/VehicleType.java
+++
@@ -1,5 +0,0 @@
-package com.example.prif233.model;
-
-public enum VehicleType {
-    CAR, SCOOTER, BIKE, BY_FOOT
-}
